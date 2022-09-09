1f05d52b4f1609829a92d73ba71fc6daff636c33
BRUNO FUCHS SANTOS DA SILVA <bfss21@inf.ufpr.br>
2022-09-09 19:00:39 -0300

    AAAAAAAAAAA

--- liblef.c
+++ liblef.c
@@ -1,1 +1,41 @@
 #include "liblef.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/* # Lista de futuros eventos# */
+
+/* 
+ * Cria uma LEF vazia 
+ */
+lef_t * cria_lef ()
+{
+    
+    lef_t* lef;
+    nodo_lef_t* nodo_lef;
+
+    if (!(lef = malloc(sizeof(lef_t))))
+        return NULL;
+
+    nodo_lef_t* novo;
+    if(!(novo = cria_nodo_lef())){
+        free(lef);
+        return NULL;
+    }
+
+    lef->Primeiro = NULL;
+    
+    return lef;
+
+}
+
+nodo_lef_t* cria_nodo_lef()
+{
+    nodo_lef_t* novo;
+    if (!(novo = malloc(sizeof(nodo_lef_t))))
+        return NULL;
+    
+    novo->evento = NULL;
+    novo->prox = NULL;
+
+    return novo;
+}
